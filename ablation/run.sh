c184e368aa9acf12fe1c2151c272513ada538e07
chhzh123 <hc676@cornell.edu>
2024-01-20 16:27:54 +0000

    Fix ablation

--- ablation/run.sh
+++ ablation/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp ablation.py ../
+cd ../
 python3 ablation.py
-python3 ../plot/ablation-1b.py ../script/ablation.csv
-mv ../plot/ablation-1b.pdf .
+python3 plot/ablation-1b.py ablation.csv
+mv ablation-1b.pdf script/
